66dfccde332e36f55d42231c4c10060b482555bc
Terry Yuen <terryxlife@gmail.com>
2018-02-22 01:31:29 +0800

    Refactor bitfinex whales

--- bitfinex/whales.js
+++ bitfinex/whales.js
@@ -14,8 +14,9 @@ const getTotal = (size, price) => {
 const next = (socket) => {
   return socket.next(JSON.stringify({
     event: 'subscribe',
-    channel: 'trades',
-    pair: `${currency.toUpperCase()}USD`
+    channel: 'book',
+    pair: `${currency.toUpperCase()}USD`,
+    prec: 'R0'
   }))
 }
 
@@ -27,24 +28,30 @@ const stream = () => {
 
   websocket
     .filter(res => Array.isArray(res))
-    .filter(res => res[1] === 'te')
-    .map(res => res[2])
+    .map(res => res[1])
+    .skip(1)
+    .filter(row => row[1] > 0)
+    .filter(row =>
+      numeral(row[2]).value() >= config.bitfinex.currency_amount[currency.toLowerCase()].positive ||
+      numeral(row[2]).value() <= config.bitfinex.currency_amount[currency.toLowerCase()].negative
+    )
+    .filter(row =>
+      getTotal(row[2], row[1]).value() >= config.bitfinex.filter_buy_amount ||
+      getTotal(row[2], row[1]).value() <= config.bitfinex.filter_sell_amount
+    )
     .map(data => {
       const point = []
-      const time = moment(data[1]).format('X')
+      const time = moment().format('X')
       const size = data[2]
-      const price = data[3]
-      const total = getTotal(size, price)
+      const price = data[1]
+      const total = getTotal(size, price).value()
 
       point.push(time, total)
 
       return point
     })
-    .filter(point => point[1].value() >= config.bitfinex.filter_buy_amount || point[1].value() <= config.bitfinex.filter_sell_amount)
-    .map(point => [point[0], point[1].format('0.00')])
     .bufferTime(5000)
     .filter(points => points.length > 0)
-    .do(console.info)
     .mergeMap(points => Observable.fromPromise(datadog.send([
       {
         metric: `bitfinex.${argv.env}.${currency.toLowerCase()}.whales`,
@@ -55,7 +62,7 @@ const stream = () => {
       }
     ])))
     .subscribe(
-      () => {},
+      console.info,
       (err) => {
         console.error(err.message)
         websocket.complete()
