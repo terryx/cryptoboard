423c78675293c8900efe3bbd4079bd29dfe47b38
Terry Yuen <terryxlife@gmail.com>
2018-02-23 10:38:37 +0800

    Refactor gdax

--- gdax/whales.js
+++ gdax/whales.js
@@ -7,61 +7,68 @@ const notification = require('../utils/notification')
 const numeral = require('numeral')
 const Gdax = require('gdax')
 const currency = argv.currency
-const websocket = new Gdax.WebsocketClient([`${currency.toLowerCase()}-USD`])
+const websocket = new Gdax.WebsocketClient([`${currency.toUpperCase()}-USD`])
 
 const getTotal = (size, price) => {
   return numeral(size).multiply(numeral(price).value())
 }
 
-Observable
-  .fromEvent(websocket, 'message')
-  .filter(res => res.type === 'received')
-  .distinct(res => res.order_id)
-  .filter(res => numeral(res.size).value() >= config.gdax.currency[currency])
-  .filter(res => getTotal(res.size, res.price).value() >= config.gdax.filter_amount)
-  .do(res => {
-    const total = getTotal(res.size, res.price)
+const stream = () => {
+  Observable
+    .fromEvent(websocket, 'message')
+    .filter(res => res.type === 'received')
+    .distinct(res => res.order_id)
+    .filter(res => numeral(res.size).value() >= config.gdax.currency[currency])
+    .filter(res => getTotal(res.size, res.price).value() >= config.gdax.filter_amount)
+    .do(res => {
+      const total = getTotal(res.size, res.price)
 
-    if (total.value() >= config.gdax.notify_amount) {
-      const message = `
-GDAX ${res.product_id}
-<b>${res.side.toUpperCase()}</b> ${numeral(res.size).format('0.00')} at ${total.format('$0.00a')}
-`
-      notification.sendMessage(config.telegram.bot_token, config.telegram.channel_id, message)
-    }
+      if (total.value() >= config.gdax.notify_amount) {
+        const message = `
+  GDAX ${res.product_id}
+  <b>${res.side.toUpperCase()}</b> ${numeral(res.size).format('0.00')} at ${total.format('$0.00a')}
+  `
+        notification.sendMessage(config.telegram.bot_token, config.telegram.channel_id, message)
+      }
 
-    return res
-  })
-  .map(res => {
-    const total = getTotal(res.size, res.price)
-    const point = []
+      return res
+    })
+    .map(res => {
+      const total = getTotal(res.size, res.price)
+      const point = []
 
-    point.push(moment(res.time).format('X'))
+      point.push(moment(res.time).format('X'))
 
-    const side = res.side.toUpperCase()
+      const side = res.side.toUpperCase()
 
-    if (side === 'BUY') {
-      point.push(total.format('0.00'))
-    }
+      if (side === 'BUY') {
+        point.push(total.format('0.00'))
+      }
 
-    if (side === 'SELL') {
-      point.push(numeral(0).subtract(total.value()).format('0.00'))
-    }
+      if (side === 'SELL') {
+        point.push(numeral(0).subtract(total.value()).format('0.00'))
+      }
 
-    return point
-  })
-  .bufferTime(5000)
-  .filter(res => res.length > 0)
-  .mergeMap(points => Observable.fromPromise(datadog.send([
-    {
-      metric: `gdax.${argv.env}.${currency}.whales`,
-      points: points,
-      type: 'gauge',
-      host: 'api.gdax.com',
-      tags: [`gdax:${argv.env}`]
-    }
-  ])))
-  .subscribe(
-    console.info,
-    console.error
-  )
+      return point
+    })
+    .bufferTime(5000)
+    .filter(res => res.length > 0)
+    .do(points => Observable.fromPromise(datadog.send([
+      {
+        metric: `gdax.${argv.env}.${currency}.whales`,
+        points: points,
+        type: 'gauge',
+        host: 'api.gdax.com',
+        tags: [`gdax:${argv.env}`]
+      }
+    ])))
+    .subscribe(
+      console.info,
+      (err) => {
+        console.error(err)
+        stream()
+      }
+    )
+}
+
+stream()
